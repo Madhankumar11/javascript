e1d29d6e617c840bb43d4d4fd467e9ec88fe2d0b
Madhankumar11 <madhankumarraja6@gmail.com>
2023-11-08 18:59:42 -0800

    form is changed

--- To-Do list/form.js
+++ To-Do list/form.js
@@ -1,8 +1,8 @@
 const form = document.querySelector('#form')
 const username = document.querySelector('#username');
-
+const email = document.querySelector('#email');
 const password = document.querySelector('#password');
-
+const cpassword = document.querySelector('#cpassword');
 
 form.addEventListener('submit',(e)=>{
     
@@ -13,9 +13,9 @@ form.addEventListener('submit',(e)=>{
 
 function validateInputs(){
     const usernameVal = username.value.trim()
-
+    const emailVal = email.value.trim();
     const passwordVal = password.value.trim();
-    
+    const cpasswordVal = cpassword.value.trim();
     let success = true
 
     if(usernameVal===''){
@@ -26,7 +26,17 @@ function validateInputs(){
         setSuccess(username)
     }
 
-   
+    if(emailVal===''){
+        success = false;
+        setError(email,'Email is required')
+    }
+    else if(!validateEmail(emailVal)){
+        success = false;
+        setError(email,'Please enter a valid email')
+    }
+    else{
+        setSuccess(email)
+    }
 
     if(passwordVal === ''){
         success= false;
@@ -40,9 +50,17 @@ function validateInputs(){
         setSuccess(password)
     }
 
-   
-
-    
+    if(cpasswordVal === ''){
+        success = false;
+        setError(cpassword,'Confirm password is required')
+    }
+    else if(cpasswordVal!==passwordVal){
+        success = false;
+        setError(cpassword,'Password does not match')
+    }
+    else{
+        setSuccess(cpassword)
+    }
 
     return success;
 
@@ -66,4 +84,10 @@ function setSuccess(element){
     inputGroup.classList.remove('error')
 }
 
-
+const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
